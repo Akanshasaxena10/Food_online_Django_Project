c96642f7e15c1aa3cda54ef8515bd02bf1722f7b
akansha <akanshasaxena10@gmail.com>
2023-09-06 22:12:27 +0200

    registration form added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.tokens import default_token_generator
+from .forms import UserForm
 
 # Create your views here.
+def registerUser(request):
+    if request.method == 'POST':
+        
+        print(request.POST) 
+        form = UserForm(request.POST)
+        if form.is_valid():
+             form.save()
+             return redirect('registerUser')
+    else:   
+       form = UserForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
